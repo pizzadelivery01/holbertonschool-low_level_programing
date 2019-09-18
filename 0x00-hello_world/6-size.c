a135a289458cee53e39a5009ed0c587cd8c476e7
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-18 22:37:09 +0000

    real final commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	printf("Size of a char: %lu bytes (s)\n", sizeof(char));
 	printf("Size of a int: %lu bytes (s)\n", sizeof(int));
 	printf("Size of a long: %lu bytes (s)\n", sizeof(long));
+	printf("Size of a long long: %lu bytes (s)\n", sizeof(long long));
 	printf("Size of a char: %lu bytes (s)\n", sizeof(float));
 	return (0);
 }
