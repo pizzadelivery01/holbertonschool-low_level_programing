578a28f287d685e9170d10973adff4000a9db8d1
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-18 21:29:11 +0000

    changed \, and [NUM]

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char str[] = "with proper grammar, but the outcome is a piece of art\,";
+	char str[60] = "with proper grammar\, but the outcome is a piece of art\,";
 
 	printf("%s\n", str);
 	return (0);
