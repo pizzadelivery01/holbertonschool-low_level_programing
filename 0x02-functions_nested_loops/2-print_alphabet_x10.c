06f6fce06c2803a8291cd9f4dc70e62ba9c9b984
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-24 23:20:06 +0000

    updated header and made alphabet loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Desctiption: print_alphabet_x10 will print the english alphabet ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int letter = 'a';
+	int repeat = 1;
+
+	while (repeat <= 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	}
+	_putchar('\n');
+}
