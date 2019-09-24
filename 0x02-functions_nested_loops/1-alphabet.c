a79ef678f655627798f475822348338157b4c0c2
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-24 22:08:36 +0000

    updated header and print_alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet will print the english alphabet in lowercase
+ *
+ *no return void
+ */
+
+void print_alphabet(void)
+{
+	letter = 'a';
+
+	while( letter <= 'z')
+	{
+		_putchar( letter);
+		letter++;
+	}
