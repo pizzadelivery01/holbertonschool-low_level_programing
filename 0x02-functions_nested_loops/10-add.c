ed1cf0f75ab901312c220c60e11a7f2c41a370c7
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-25 03:30:35 +0000

    files at least fo rpartial credit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - adds integers
+ * @a: int
+ * @b: int
+ * Desrciption: adds integers
+ *
+ * Return: int
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print 24 hours by min
+ *
+ * Description: prints every min of the day
+ *
+ * Return: 0 always
+ */
+
+void jack_bauer(void)
+{
+	int first_hour = 0;
+	int second_hour = 0;
+	int first_min = 0;
+	int second_min = 0;
+
+	while (first_hour < 3)
+	{
+		while (second_hour < 5)
+		{
+			while (first_min < 7)
+			{
+				while (second_min <= 9)
+				{
+					_putchar(first_hour);
+					_putchar(second_hour);
+					_putchar(':');
+					_putchar(first_min);
+					_putchar(second_min);
+					_putchar('\n');
+					second_min++;
+				}
+				first_min++;
+			}
+			second_hour++;
+		}
+		first_hour++;
+	}
+
+}
