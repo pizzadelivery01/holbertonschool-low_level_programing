724cdaef3c32e6768cf19b1280a96270b1433d71
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-25 01:44:29 +0000

    updated header and added possitive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign - determines if number is positive or negative
+ * @n: int
+ * Description: determines if number is + or -
+ *
+ * Return: variable dependant
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,4 @@
-#ifndef holberton_h
+B#ifndef holberton_h
 #define holberton_h
 
 int _putchar(char c);
@@ -6,5 +6,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 
 #endif
