e34e9cc61168d3859606c553e64a419c97e6c865
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-25 02:31:41 +0000

    last digit f(x)

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit -  prints last digit of number
+ * @input: number inputed
+ *
+ * Discription: function for finding just last number of number
+ *
+ * Return: integer
+ */
+
+int print_last_digit(int input)
+{
+	return (input % 10);
+}
