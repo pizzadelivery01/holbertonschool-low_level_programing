18a03d9e93b1f82d0d7c5145382289fd1362b5bb
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-26 19:38:47 +0000

    deleted spaces

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 
 /**
- *
  * _isupper - entry point
  * @c: int
  *
@@ -12,7 +11,7 @@
 
 int _isupper(int c)
 {
-	if (( c >= 'A') && ( c <= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
 		return (0);
