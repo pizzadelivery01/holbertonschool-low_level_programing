7293db2acf0c8a3c4c5971153924e56be4fa8da3
Jeremy Bedolla <jjbedolla@gmail.com>
2019-09-26 19:36:04 +0000

    updated header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,7 +1,9 @@
 #ifndef holberton_h
 #define holberton_h
 
-void(more_numbers)(void);
+void print_most_numbers(void);
+void print_numbers(void);
+void more_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _putchar(char c);
