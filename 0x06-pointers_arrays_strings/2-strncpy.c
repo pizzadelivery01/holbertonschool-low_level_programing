98a8f98cc148b6a68b815488ce3f4e950ddb48c8
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-03 20:04:06 +0000

    strncpy added to header

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton"
+/**
+ * *_strncpy - copies string to destination
+ * @n: int
+ * @dest: char
+ * @src: char
+ *
+ * Description: copies string to destination
+ *
+ * Return: pointer to string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	while ((i < n) && (src[i] != '\0'))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
