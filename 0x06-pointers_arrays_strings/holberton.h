019720194dd3bd6a8275e07e49601c33345de0e3
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-03 22:02:33 +0000

    updated header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef holberton_h
 #define holberton_h
 
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
