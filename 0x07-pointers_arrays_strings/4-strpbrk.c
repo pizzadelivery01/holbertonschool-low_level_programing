6ee12e381c5e9ba43504f6e3ded30e5b7122bce8
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-08 01:56:38 +0000

    adding strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strpbrk - locates bytes in string
+ * @s: input string
+ * @accept: matching character
+ *
+ * Description: looks for matching byte and returns pointer to it
+ *
+ * Return: pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while ((*s != '\0') && (s != NULL))
+	{
+		i = 0;
+		while (*accept[i] != '\0')
+		{
+			if (*s == accept[i])
+			return (s);
+		i++;
+		s++;
+		}
+	}
+	if (accept == '\0')
+		return (s);
+	return (NULL);
+}
