d9b6078707d776564c469236577226d18409f75f
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-14 01:43:31 +0000

    added lines

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the number of args to program
  * @argc: number of args
@@ -6,6 +7,7 @@
  * Description: prints number of args passed into program
  * Return: 0
  */
+
 int main(int argc, *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
