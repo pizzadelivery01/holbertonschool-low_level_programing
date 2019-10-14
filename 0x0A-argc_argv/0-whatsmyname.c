11fd545dc5dc8a50d11931228faa95a0adeff41a
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-14 01:34:04 +0000

    added args to main function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  * Description: prints name of program
  * Return: function name
  */
-int main(int argc, char *argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	(void)argc;
 	return (0);
 }
