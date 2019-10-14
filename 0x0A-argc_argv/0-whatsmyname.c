1cb2c108ff74d2e5d77a99f2529bef04a0651b9b
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-14 01:18:39 +0000

    adding first files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * @argc: amount of args
+ * @argv: array of args
+ * Description: prints name of program
+ * Return: function name
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
