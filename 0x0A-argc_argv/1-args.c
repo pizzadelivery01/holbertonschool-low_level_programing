9090fc52c31ab76c35bbcc8ef2979e06a0c0de54
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-14 01:40:56 +0000

    adding argc function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of args to program
+ * @argc: number of args
+ * @argv: array of args
+ * Description: prints number of args passed into program
+ * Return: 0
+ */
+int main(int argc, *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
