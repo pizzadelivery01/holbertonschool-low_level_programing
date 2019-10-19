cf5c4db49b0159efae29720909555617cfe4f243
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-19 02:06:20 +0000

    betty fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	while (i < (nmemb * size))
 	{
-		((char*)ptr)[i] = 0;
+		((char *)ptr)[i] = 0;
 		i++;
 	}
 	return ((void *)ptr);
