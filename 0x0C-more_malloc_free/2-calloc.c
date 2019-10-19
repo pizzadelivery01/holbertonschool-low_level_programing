ed544c3221dab279ce6d7c353f2d1af4d91e5c36
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-19 00:53:46 +0000

    adding calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _calloc - allocates memory and files with 0
+ * @nmemb: size of array
+ * @size: size of memory block
+ * Description: building calloc
+ * Return: pointer or NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < (nmemb * size))
+	{
+		ptr[i] = '0';
+		i++;
+	}
+	return ((void *)ptr);
+}
