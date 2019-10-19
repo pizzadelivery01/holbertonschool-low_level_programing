e8a0202337fa37ec10c3fd878849f41f3f9eff73
Jeremy Bedolla <jjbedolla@gmail.com>
2019-10-19 02:33:16 +0000

    adding concat

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,7 @@
 #ifndef holberton_h
 #define holberton_h
 
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
