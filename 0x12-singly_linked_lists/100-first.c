180c84b36af78d5cd92c37ea50f3ad0c15ec6cd7
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-01 18:07:14 +0000

    added second printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@ void pre_main(void) __attribute__ ((constructor));
  */
 void pre_main(void)
 {
-	printf("[%s]";
-"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
