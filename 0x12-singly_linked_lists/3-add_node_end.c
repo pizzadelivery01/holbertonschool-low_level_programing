2f98c05936f9684b8366dd91a4323f719c864c4d
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-01 17:45:44 +0000

    added dereference

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,7 +26,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *temp;
 
-	new = malloc(sizeof(list_t));
+	*new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
