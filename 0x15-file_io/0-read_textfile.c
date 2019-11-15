9294df13cd2484c89a9a0ccdc7890584009941f9
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-15 00:52:26 +0000

    adding function to repo

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,20 +17,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	Rcount = read(fd, *buff, letters);
+	Rcount = read(fd, (char *)buff, letters);
 	if (Rcount == -1)
 	{
 		close(fd);
 		free(buff);
 		return (0);
 	}
-	Pcount = write(STDOUT_FILENO, buff, Rcount);
+	Pcount = write(STDOUT_FILENO, (char *)buff, Rcount);
 	if (Pcount == -1)
 	{
 		close(fd);
