26d1cfe01517ffb6671a6475277840731ca367a1
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-07 16:01:20 +0000

    addded head null clause

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	if (*head == NULL)
+	{
+		new->n =n;
+		new->next = NULL;
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
