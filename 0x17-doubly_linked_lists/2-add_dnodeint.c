720c9de32efc97951d437a675b2ccbea9ca53fea
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-07 16:06:42 +0000

    added space

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	if (*head == NULL)
 	{
-		new->n =n;
+		new->n = n;
 		new->next = NULL;
 		new->prev = NULL;
 		*head = new;
