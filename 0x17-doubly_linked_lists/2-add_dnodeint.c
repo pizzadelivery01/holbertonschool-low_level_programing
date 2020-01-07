81fb9a9af1398869865b4f277e429cc19d3e3fc2
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-07 16:14:55 +0000

    added prev link form first node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,9 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *current;
+
+	current = *head;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -26,6 +29,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new->n = n;
 		new->next = *head;
 		new->prev = NULL;
+		currnt->prev = new;
 		*head =  new;
 	}
 	return (*head);
