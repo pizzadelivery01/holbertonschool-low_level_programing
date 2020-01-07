36d2687a2d9df72b33498d7a597962b31c56a352
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-07 16:04:11 +0000

    added brackets

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,10 +22,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = new;
 	}
 	else
-
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	*head =  new;
+	{
+		new->n = n;
+		new->next = *head;
+		new->prev = NULL;
+		*head =  new;
+	}
 	return (*head);
 }
