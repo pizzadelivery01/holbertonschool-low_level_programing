a46f6af9ebeec1f67dfbe2dd8dc12e0ad4801407
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-07 15:09:43 +0000

    adding list counter

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * size_t dlistint_len - counts number of elements in a list
+ * @h: point to head of list
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	dlistint_t *temp = NULL;
+
+	temp = (dlistint_t *) h;
+	while (temp)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
