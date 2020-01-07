d419edf3a5115c45602085aa1bf7fd51e393e0c5
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-07 18:50:33 +0000

    adding functions

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * get_dnodeint - gets node at index
+ * @head: pointer to top of linked list
+ * @index: index of node wanted
+ * Return: pointer to indexed node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count = 0;
+
+	temp = malloc(sizeof(dlistint_t));
+	temp = head;
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (index > count)
+		return(NULL);
+	count = 0;
+	temp = head;
+	while (count < index)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (temp);
+}
