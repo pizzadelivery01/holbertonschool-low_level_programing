56a57c85422fd7d54693fb16abf8b2631f10daee
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-20 01:49:07 +0000

    adding files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - creates a hash table
  * @size: size of the array
  *
- * Return: pointer to the newly created hash table
+ * Return: pointer to hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
