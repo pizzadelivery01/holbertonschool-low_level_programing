ec714462803c6f8687753f00018891d33a0e35a5
Jeremy Bedolla <jjbedolla@gmail.com>
2020-01-20 02:30:21 +0000

    variable typ0

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (flag == 1)
 				printf(", ");
-			printf("'%s': '%s'", temp->key, tmp->value);
+			printf("'%s': '%s'", temp->key, temp->value);
 			flag = 1;
 			temp = temp->next;
 		}
