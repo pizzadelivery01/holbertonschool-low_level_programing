5e4531f82047d715e8de6c7218ab4e2a4f59440d
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-15 16:29:12 -0400

    updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,16 +10,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+       size_t i;
+
+
 	if (array)
 		for (i = 0; i <= size; i++)
 		{
-			printf("Value checked array[{}] = [{}]".format(
-				       i, array[i]));
+			printf("Value checked array[%li] = [%d]\n", i, array[i]);
 			if (value ==  array[i])
-			{
-				printf("Found {} at index: {}".format(value, i));
 				return (i);
-			}
 		}
 	return (-1);
 }
